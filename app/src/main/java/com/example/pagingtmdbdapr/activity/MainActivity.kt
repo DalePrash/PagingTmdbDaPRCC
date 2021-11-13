c5161767b52b63c1ca3353aa48d8984c5ddd13cf
Dalesh Prashar <prashdale@gmail.com>
2021-11-13 12:29:41 -0600

    UI working

--- app/src/main/java/com/example/pagingtmdbdapr/activity/MainActivity.kt
+++ app/src/main/java/com/example/pagingtmdbdapr/activity/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.pagingtmdbdapr.activity
 
+
+import android.graphics.Movie
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pagingtmdbdapr.R
+import com.example.pagingtmdbdapr.adapter.MovieAdapter
 import com.example.pagingtmdbdapr.pojo.MovieList
+import com.example.pagingtmdbdapr.pojo.Movies
 import com.example.pagingtmdbdapr.service.NetworkService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val recyclerView :RecyclerView by lazy{
+        findViewById(R.id.recy) as RecyclerView
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView.setLayoutManager(object : LinearLayoutManager(this@MainActivity) {
+
+        })
         serviceCallB()
     }
 /*
@@ -43,13 +58,21 @@ class MainActivity : AppCompatActivity() {
     fun serviceCallB() {
         val networkService = NetworkService.getService()
         val callx = networkService.getMovies()
-        callx.enqueue(object :Callback<MovieList>{
+        callx.enqueue(object : Callback<MovieList> {
             override fun onResponse(call: Call<MovieList>, response: Response<MovieList>) {
-                Log.d("aaa", "Number of movies received: " )
+                Log.d("aaa", "Number of movies received: ")
+                val statusCode = response.code()
+             //   val moviesx: List<Movies> = response.body()
+                val moviesx = response.body()?.movies
+
+                recyclerView.adapter = MovieAdapter(
+                    moviesx, this@MainActivity)
+
+
             }
 
             override fun onFailure(call: Call<MovieList>, t: Throwable) {
-                Log.d("aaa", "Number of movies received: " )
+                Log.d("aaa", "Number of movies received: ")
             }
 
         })
---
+++ app/src/main/java/com/example/pagingtmdbdapr/adapter/MovieAdapter.java
@@ -0,0 +1,55 @@
+package com.example.pagingtmdbdapr.adapter;
+
+import android.content.Context;
+import android.graphics.Movie;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pagingtmdbdapr.R;
+import com.example.pagingtmdbdapr.pojo.Movies;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieHolder> {
+    private List<Movies> movies;
+    private Context context;
+
+    public MovieAdapter(List<Movies> movies, Context context) {
+        this.movies = movies;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_page, parent, false);
+        return new MovieHolder(view);    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+        holder.movieTitle.setText(movies.get(position).getMovieTitle());
+        holder.movieLanguage.setText(movies.get(position).getMovieLanguage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 6;
+    }
+
+    public static class MovieHolder extends RecyclerView.ViewHolder {
+        TextView movieTitle;
+        TextView movieLanguage;
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+            movieTitle = itemView.findViewById(R.id.movieTitle);
+            movieLanguage = itemView.findViewById(R.id.movieLanguage);
+
+        }
+    }
+}
